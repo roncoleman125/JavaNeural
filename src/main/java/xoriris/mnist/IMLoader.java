b4e5eca6c81c49df6cb41daae88c06bcb6c0058c
roncoleman <ronncoleman@gmail.com>
2022-07-16 09:28:28 -0400

    Simplified the project.

--- src/main/java/xoriris/mnist/IMLoader.java
+++ src/main/java/xoriris/mnist/IMLoader.java
@@ -3,11 +3,36 @@ package xoriris.mnist;
 import java.io.IOException;
 
 /**
- * Interface
+ * Interface to load the MNIST data.
+ * <p>There is one instance for each sample, ie, one for the training and one for the testing database.</p>
+ * @author Ron.Coleman
  */
 public interface IMLoader {
-    public MDigit[] getDigits() throws IOException;
+    ////////////////
+    // TODO: Add a constructor which takes the pixel and label paths.
+    ////////////////
+
+    /**
+     * Gets the pixel and label data in row-major order.
+     * @return Data in row-major order.
+     */
+    public double[][] load();
+
+    /**
+     * Gets the pixel magic number.
+     * @return Magic number
+     */
     public int getPixelsMagic();
+
+    /**
+     * Gets the label magic number.
+     * @return Magic number
+     */
     public int getLabelsMagic();
+
+    /**
+     * Gets the checksum.
+     * @return Checksum
+     */
     public long getChecksum();
 }
