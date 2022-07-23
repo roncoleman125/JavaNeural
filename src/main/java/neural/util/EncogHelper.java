cb7d58e0ef22e0340c22a46d59874e80d4404097
Ron.Coleman <ronncoleman@gmail.com>
2022-07-23 10:17:23 -0400

    Fixed bug in logger.

--- src/main/java/neural/util/EncogHelper.java
+++ src/main/java/neural/util/EncogHelper.java
@@ -152,7 +152,7 @@ public class EncogHelper {
 
         if(done && error < TOLERANCE)
             System.out.println("--- CONVERGED!");
-        else if((sameExceeded || epoch >= MAX_EPOCHS) && done)
+        else if((sameExceeded || epoch > MAX_EPOCHS) && done)
             System.out.println("--- DID NOT CONVERGE!");
     }
 }
