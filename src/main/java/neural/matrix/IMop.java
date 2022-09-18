35f8ac856de9f78ddde9ed4895854186ef65a020
Ron.Coleman <ronncoleman@gmail.com>
2022-09-18 12:12:55 -0400

    Improved the javadoc.

--- src/main/java/neural/matrix/IMop.java
+++ src/main/java/neural/matrix/IMop.java
@@ -1,8 +1,27 @@
 package neural.matrix;
 
+/**
+ * Matrix operations
+ * @author Ron.Coleman
+ */
 public interface IMop {
+    /**
+     * Slices a matrix horizontally.
+     * @param src Source matrix in row-major order.
+     * @param startRow Starting row
+     * @param endRow Ending row (exclusive)
+     * @return Sliced matrix in row-major order.
+     */
     double[][] slice(double[][] src,int startRow,int endRow);
-    double[][] transpose(double[][] src);
+
+    /**
+     * Dices a matrix vertically.
+     * @param src Source matrix in row-major order.
+     * @param startCol Starting row
+     * @param endCol Ending row (exclusive)
+     * @return Diced matrix in row-major order.
+     */
     double[][] dice(double[][] src,int startCol, int endCol);
+    double[][] transpose(double[][] src);
     void print(String msg, double[][] src);
 }
